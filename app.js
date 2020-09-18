b02a9130b485bf69eafef0f38650de9bbfa5c303
NikolaGolubovic <nikolagolubovic985@gmail.com>
2020-09-18 15:32:27 +0200

    uibuild

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(
     directives: {
       defaultSrc: ["'self'"],
       fontSrc: ["https://fonts.googleapis.com/", "https://fonts.gstatic.com"],
+      connectSrc: ["'self'"],
       styleSrc: [
         "'self'",
         "'unsafe-inline'",
